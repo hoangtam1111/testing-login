cbfd97e05474c76f31656d2e0104884d6cc31504
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2024-08-18 11:18:33 +0700

    using jwt login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login'])->name('postLogin');
+    Route::post('/register', [AuthController::class, 'register'])->name('postRegister');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('postLogout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('postRefresh');
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::post('/change-pass', [AuthController::class, 'changePassWord']);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
